6d39367b2ce711311d8058c3d5323d7c5d90eb1e
Matthew Lazarowitz <mlazarowitz@nvidia.com>
2024-09-06 11:39:52 -0700

    Clean up, and improving code
    Making some changes based on addiitonal learnings from experiments.
    Trying to improve the readme.

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-This is just a rough description for this right now.
-The protocol for the Vornado Transom fan is very simple. One bit consists of a lang mark followed by a short space, and the otehr is a short mark followed by a long space. 
-If I arbitraranny name the long mark bit as a 1 and the otehr a s a 0 then the bits are as follows:
-1: ~1300 uS mark followed by a ~400uS space.
-0: ~400uS mark followed by a ~1300 uS space.
+Basic demonstration of emulating the IR remote for a Vornado Transom window fan.
+This sketch is based around an ESP8266 and uses the IrRemoteESP8266 library. Code will need porting for other libraries.
 
-It uses a standard 38khz carrier frequency for the remote. 
+In a nutshell, the fan's protocol looks like it's PWM based with a bit period of about 1680 microseconds and uses a standard 38KHz carrier frequency.
+The two bits are a 75% duty cycle and a 25% duty cycle. Some slop is allowed.
 
+However, to use the irsend.sendRaw() method, a command is defined as a set of marks and spaces. 
+So the 75% duty cycle is defined as  a 1260us mark and 400us space. The 25% ducty cycle is defined as a 420us mark and 1260us space.
+TODO: make 75% bit's space 420us and see if it breaks anything
 
-A command is a 12 bit pattern that repeats every 8ms. represented as hex, the commands are as follows:
-Power: 0xD84
-up arrow: 0xDC6
-down arrow: 0xD82
-airflow direction (top of the circle on the remote): 0xD81
-Thermometer icon: 0xD82
+For the this sketch, the 75% duty cycle bit will be assigned as a 1 and the 20% duty cycle as a 0.
 
-This repo implements this but it's not yet working consistantly. I think the issue is that I need to drive the LED with a transistor rather than try to rely on the ESP8266 directly.
+Commands are 3 nibbles and need to be repeated at least twice. 
+The gap between repeats of the command cannot be longer than about 7ms or it will be interpreted as a separate button press. 
+A gap of about 9ms is enough for a second command to be sent while ensuring the fan's control panel doesn't sleep. 
+
+Any correctly formatted 12 bit combination will make the fan attemt to decode it whether the command is valid or not.
+This is useful for waking the fan's control panel before sending the desired command.
+
+The following comamnds were determined by capturing the waveform across the IR LED of the offical remote with 1s and 0s defined as above.
+The commands are recorded as 3 digit hexadecimal numbers.
+
+Power button : 0xD84
+Up arrow button : 0xDC6
+Down arrow button : 0xD82
+Direct mode button : 0xD81
+Auto mode button (thermometer icon) : 0xDC3
+
+I have not been able to deterine if there is an additional encoding scheme to these commands
+
+A little fuzzing has determined some alternatives for some commands (usally 1 bit of difference in the last nibble)
+Power button alternative: 0xD85
+Down Arrow alternative: 0xD83
+
+TODO: Automated fuzzing differnet 3 digit commnands of the interface to identfy any additional commands. 
+I would like to determine if there are comands to explicityly set exhaust and direct modes rather than toggle between them (to enable setting a known state)
+It would be nice if there was a better way to set the temperature in auto mode besides using the up and down arrows. The range of 60 to 90 (fahrenheit) means 'homing' the auto control value could take some time. 
+
+There may be more to this protocol such as longer commands but that's not something I will persue.
+For now, this will meet my goals of being able to turn the fan on and off and adust it's speed via an ESP8266 and MQTT. 
+Auto control won't be used as I will use a home automation framework and additional temperature sensors to control when the fan runs and at what speed.
--- vornado_transom_remote_test.ino
+++ vornado_transom_remote_test.ino
@@ -13,13 +13,13 @@
 #define HEX_3 BIT_ZERO, BIT_ZERO, BIT_ONE, BIT_ONE
 #define HEX_2 BIT_ZERO, BIT_ZERO, BIT_ONE, BIT_ZERO
 #define HEX_1 BIT_ZERO, BIT_ZERO, BIT_ZERO, BIT_ONE
-#define END_GAP 8000
 
-const uint16_t power_button[24] = {HEX_D,HEX_8,HEX_4};
+const uint16_t power_button[24] = {HEX_D,HEX_8,HEX_4}; //d85 is also power
 const uint16_t airflow_dir[24] = {HEX_D,HEX_8,HEX_1};
 const uint16_t temp_ctrl[24] = {HEX_D,HEX_C,HEX_3};
 const uint16_t up_arrow[24] = {HEX_D, HEX_C, HEX_6};
-const uint16_t down_arrow[24] = {HEX_D, HEX_8, HEX_2};
+const uint16_t down_arrow[24] = {HEX_D, HEX_8, HEX_2}; //d83 also works
+const uint16_t dummy[24] = {HEX_D, HEX_D, HEX_D}; 
 
 // GPIO to use to control the IR LED circuit. Recommended: 4 (D2).
 const uint16_t kIrLedPin = 4;
@@ -29,69 +29,65 @@ const uint16_t kFrequency = 38000;
 
 IRsend irsend(kIrLedPin, false);
 
-void sendPower(){
-  irsend.sendRaw(power_button,sizeof(power_button)/sizeof(uint16_t),kFrequency);
-  delay(8);
-  irsend.sendRaw(power_button,sizeof(power_button)/sizeof(uint16_t),kFrequency);
-  delay(8);
-  irsend.sendRaw(power_button,sizeof(power_button)/sizeof(uint16_t),kFrequency);
-  delay(8);
-  irsend.sendRaw(power_button,sizeof(power_button)/sizeof(uint16_t),kFrequency);
-
-}
-
-void sendMyData (const uint16_t *data, int data_size, int repeat,int gap){
-  Serial.printf("sending %d entries of data %d times. Gap of %d ms\n",data_size, repeat, gap);
+void sendMyData (const uint16_t *data, int data_size, int repeat){
+  Serial.printf("sending %d entries of data %d times. Gap of %d ms\n",data_size, repeat);
   Serial.println("to send: ");
   for (int i=0; i<data_size; i++){
     Serial.printf("%d ",data[i]);
   }
-  //irsend.sendRaw(warm_up,2,38000);
   Serial.println();
+  // The fan can 'sleep' the control pannel and need to be worken up.
+  // We don't want to use any actual commands in case the control panel isn't asleep.
+  // It turns out that an invalid, but 'properly formatted' pulse train will wake the panel.
+  // If the panel isn't sleeping, this is a no-op.
+  // If the panel is sleeping, this will wake it.
+  // The command needs to be sent twice. The first looks like it's used to 
+  // get the fan's 'attention' and the second will register as an actual command attempt. 
+  irsend.sendRaw(dummy,24,kFrequency);
+  delay(7);
+  irsend.sendRaw(dummy,24,kFrequency);
+  delay(9); //a gap in transmitting longer than 8ms tells the fan that a button has been released and pressed again.
   for (int i=0; i<repeat; i++){
     irsend.sendRaw(data,data_size,kFrequency);
-    delay(gap);
+    delay(7); //a delay of 7ms seems to work for all commands. Anything lower than 6 will cause the command to be ignored.
   }
 }
 
 void setup() {
   irsend.begin();
-  Serial.begin(74880);
+  Serial.begin(115200);
 }
 
 void loop() {
   int msg_gap = 9;
   if (Serial.available() > 0) {
     int incomingByte = Serial.read(); // read the incoming byte:
-    Serial.print(" I received: ");
-    Serial.println(incomingByte);
 
     switch (incomingByte) {
       case 'p':
-      //power button
+        //power button
         Serial.println("sending power_button");
-        sendMyData(power_button,sizeof(power_button)/sizeof(uint16_t),6,7);
-        //sendPower();
+        sendMyData(power_button,sizeof(power_button)/sizeof(uint16_t),3);
         break;
       case 'u':
-      //up arrow
+        //up arrow
         Serial.println("sending up_arrow");
-        sendMyData(up_arrow,sizeof(up_arrow)/sizeof(uint16_t),6,7);
+        sendMyData(up_arrow,sizeof(up_arrow)/sizeof(uint16_t),3);
         break;
       case 'd':
       //down arrow
         Serial.println("sending down_arrow");
-        sendMyData(down_arrow,sizeof(down_arrow)/sizeof(uint16_t),6,7);
+        sendMyData(down_arrow,sizeof(down_arrow)/sizeof(uint16_t),3);
         break;
       case 'f':
       //airflow direction in/out
         Serial.println("sending airflow_dir");
-        sendMyData(airflow_dir,sizeof(airflow_dir)/sizeof(uint16_t),3,8);
+        sendMyData(airflow_dir,sizeof(airflow_dir)/sizeof(uint16_t),3);
         break;
       case 't':
       //thermometer button
         Serial.println("sending temp_ctrl");
-        sendMyData(temp_ctrl,sizeof(temp_ctrl)/sizeof(uint16_t),3,8);
+        sendMyData(temp_ctrl,sizeof(temp_ctrl)/sizeof(uint16_t),3);
         break;
       default:
         break;
